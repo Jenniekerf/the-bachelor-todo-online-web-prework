e7098c87bfbb7bd78e3fb1b37a25cae3b1868679
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-13 17:21:20 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,52 +1,75 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def get_first_name_of_season_winner(data, season)
-  data[season].each do |contestant_hash|
-    if contestant_hash["status"].downcase == "winner"
+  data[season].each {|contestant_hash|
+    if contestant_hash["status"] == "Winner"
       return contestant_hash["name"].split.first
-    end
-   end   
+   end
+  }
 end
 
 def get_contestant_name(data, occupation)
-  data.each do |season, contestants|
-    contestants.each do |contestant_hash|
+  data.each {|season, contestants|
+    contestants.each {|contestant_hash|
       if contestant_hash["occupation"] == occupation
         return contestant_hash["name"]
-      end 
-  end
-end
+      end
+    }
+  }
 end
 
 
 def count_contestants_by_hometown(data, hometown)
   counter = 0
-  data.each do |season, contestants|
-    contestants.each do |contestant_hash|
+  data.each {|season, contestants|
+    contestants.each {|contestant_hash|
      if contestant_hash["hometown"] == hometown
        counter += 1
-end
-end
-end
+     end
+    }
+  }
 counter
 end
 
 
 def get_occupation(data, hometown)
-  data.each do |season, contestants|
-    contestants.each do |contestant_hash|
+  data.each {|season, contestants|
+    contestants.each {|contestant_hash|
       if contestant_hash["hometown"] == hometown
         return contestant_hash["occupation"]
       end
-    end
-  end
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
   age_total = 0 
   counter = 0 
-  data[season].each do |contestant_hash|
+  data[season].each {|contestant_hash|
     age_total += (contestant_hash["age"]).to_i 
     counter += 1
-  end
+  }
 (age_total / counter.to_f).round(0)
 end
 
